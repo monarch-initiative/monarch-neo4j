f65682dc22f99d121d59427156ea76f3cc497c29
Faisal Alquaddoomi <faisal.alquaddoomi@cuanschutz.edu>
2023-12-19 14:13:20 -0700

    Changes arguments to load_and_boot_neo4j.sh to be the dump name and then the database name, which seems to align with the docs. Also, forces recreation of the db in case we attempt to reload into an existing instance.

--- scripts/load_and_boot_neo4j.sh
+++ scripts/load_and_boot_neo4j.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-NEO4J_DATABASE=${1:-monarch-kg}
-NEO4J_DUMP_FILENAME=${1:-${NEO4J_DATABASE}.neo4j.dump}
+DEFAULT_NEO4J_DATABASE="monarch-kg"
+
+NEO4J_DUMP_FILENAME=${1:-${DEFAULT_NEO4J_DATABASE}.neo4j.dump}
+NEO4J_DATABASE=${2:-${DEFAULT_NEO4J_DATABASE}}
 
 [ ! -z ${DO_LOAD} ] && \
     echo "Loading Neo4j dump file '$NEO4J_DUMP_FILENAME' into database '$NEO4J_DATABASE'" && \
-    neo4j-admin load --from=/dumps/$NEO4J_DUMP_FILENAME
+    neo4j-admin load --force --from=/dumps/$NEO4J_DUMP_FILENAME
 
 chown -R neo4j /data
 
